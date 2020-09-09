c4e09acf388c4aa51dc2ae053719ea92d6981e18
Nataliia <n3v3st4@gmail.com>
2020-09-09 17:51:07 +0300

    Updates

---
+++ precache-manifest.9024aa77a4f64773d893a32d258d07b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb35da7ae573baeba0278b829d86b035",
+    "url": "/react-hw-3-phonebook/index.html"
+  },
+  {
+    "revision": "bb590c6523ac2daa68d3",
+    "url": "/react-hw-3-phonebook/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "1b708d0988066cfa916b",
+    "url": "/react-hw-3-phonebook/static/css/main.5abba537.chunk.css"
+  },
+  {
+    "revision": "bb590c6523ac2daa68d3",
+    "url": "/react-hw-3-phonebook/static/js/2.a386a9d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-3-phonebook/static/js/2.a386a9d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b708d0988066cfa916b",
+    "url": "/react-hw-3-phonebook/static/js/main.400d29be.chunk.js"
+  },
+  {
+    "revision": "baa3540d5baad6746b82",
+    "url": "/react-hw-3-phonebook/static/js/runtime-main.87cf04d4.js"
+  }
+]);
